a90f497e36c9301b3046e9af2fdc3c25956fa21d
WagnerSolak <wwwagnerww@gmail.com>
2021-07-27 20:46:29 -0300

    finalizado classes dominio

--- src/main/java/com/solak/os/domain/Tecnico.java
+++ src/main/java/com/solak/os/domain/Tecnico.java
@@ -1,7 +1,20 @@
 package com.solak.os.domain;
 
-public class Tecnico extends Pessoa {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<OS> list = new ArrayList<>();
+	
 	// para instanciar os tecnicos com mais facilidade, é
 	// necessário o construtor da superclasse
 	public Tecnico() {
@@ -11,5 +24,16 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String telefone) {
 		super(id, nome, cpf, telefone);
 	}
+
+	
+	public List<OS> getList() {
+		return list;
+	}
+
+	public void setList(List<OS> list) {
+		this.list = list;
+	}
+	
+	
 
 }
