9c605a8da2256036276b5e708a1ef5e455e71b3d
WagnerSolak <wwwagnerww@gmail.com>
2021-07-30 13:26:27 -0300

    ajustes na API e Deploy

--- src/main/java/com/solak/os/resources/TecnicoResource.java
+++ src/main/java/com/solak/os/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.solak.os.domain.Tecnico;
 import com.solak.os.dtos.TecnicoDTO;
 import com.solak.os.services.TecnicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/tecnicos") // endpoint inicial para acessar os recursos do Tecnico
 public class TecnicoResource {
